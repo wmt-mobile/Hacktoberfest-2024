1e72f2dadfaff0146803dd84c4dc231d5d3d3fc5
Rohan Bangoriya <rohanb@webmobtech.com>
2024-10-11 14:28:42 +0530

    problem 2 added

--- Rohan Bangoriya/mian.py
+++ Rohan Bangoriya/mian.py
@@ -13,3 +13,20 @@ print(twoSum(nums, target))
 
 
 ## Q2. Given a string s, find the length of the longest substring without repeating characters.
+
+s = "abcabcbb"
+
+def lengthOfLongestSubstring(s):
+    n = len(s)
+    ans = 0
+    for i in range(n):
+        seen = set()
+        for j in range(i, n):
+            if s[j] in seen:
+                break
+            seen.add(s[j])
+        ans = max(ans, len(seen))
+    return ans
+
+print(lengthOfLongestSubstring(s))
+
